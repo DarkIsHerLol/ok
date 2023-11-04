b2439061422a5b66949653c68ccf92b2aa06484b
DarkIsHerLol <146543624+DarkIsHerLol@users.noreply.github.com>
2023-11-04 16:54:32 +0500

    Add files via upload

---
+++ ha.py
@@ -0,0 +1,51 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='^', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Здравствуйте! Я  {bot.user} и я обьясню вам как долго разлагается какой-либо материал.')
+
+@bot.command()
+async def bottle(ctx):
+    await ctx.send("бутылка разлагается от 100 до 200 лет")
+
+@bot.command()
+async def paper(ctx):
+    await ctx.send("бумага разлагается несколько месяцев")
+
+@bot.command()
+async def glass(ctx):
+    await ctx.send("Стекло разлагается около тысячи лет")
+
+@bot.command()
+async def paperboard(ctx):
+    await ctx.send("Картон разлагается за 1-2 месяца")
+
+@bot.command()
+async def WhatIf(ctx):
+    await ctx.send("Брошенный в природе мусор разлагается образуя Метан. Образование метана не может контролироваться, он постоянно поступает в атмосферу и часто самовоспламеняется ")
+
+@bot.command()
+async def brick(ctx):
+    await ctx.send("Кирпичи разлагаются до 100 лет")
+
+@bot.command()
+async def batteries(ctx):
+    await ctx.send("Батарейки разлагаются 200 лет")
+
+
+@bot.command()
+async def Why(ctx):
+    await ctx.send("Зачем я создан? Я создан для того, чтобы помогать незнающим людям и говорить доступную информацию чтобы люди знали что выкинуть всякие бутылки в урну это прекрасное действие а выкинуть эту же бутылку на улице это сильный вред всеё природе")
+
+
+bot.run("MTE2NTI1NTc5NzA2NDg3MjAwNg.GdU8hb.EOZI0_1k0vs38lhOkA1lg-F5U0zHIHp0cUQnXA")
